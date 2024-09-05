0fde62091892977a88b5f258de3e4e06025254c7
sungwon <aogn07@naver.com>
2024-09-05 17:55:11 +0900

    init commit

---
+++ src/main/java/com/example/clushapi/ClushApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.clushapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClushApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClushApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/clushapi/ClushApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.clushapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClushApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
